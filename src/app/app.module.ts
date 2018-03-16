5bd76a2328bd01dc2ab1ae2c3087eb3c2bb25313
williamsarrazin018 <williamsarrazin@msn.com>
2018-03-16 10:39:31 -0500

    Menu et liens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { AssetsComponent } from './assets/assets.component';
+import { ImagesComponent } from './assets/images/images.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent,
+    AssetsComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/assets/images/images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inter-images',
+  templateUrl: './images.component.html',
+  styleUrls: ['./images.component.css']
+})
+export class ImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
